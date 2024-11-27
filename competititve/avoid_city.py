f7dbd8590b73f2e76fe4f3ab458f6f26a157831f
Finix-07 <154240013+Finix-07@users.noreply.github.com>
2024-11-27 23:49:20 +0530

    bad triplet and avoid city

--- competititve/avoid_city.py
+++ competititve/avoid_city.py
@@ -1,3 +1,69 @@
+'''
+
+A. Avoid Cities
+time limit per test
+1.5 seconds
+memory limit per test
+256 megabytes
+
+There are N
+cities with M
+
+bi-directional roads between them. You want to travel from a given source city to target city. But there are some cities that you cannot visit because they are cursed.
+
+Find the smallest number of roads that you should take to complete your travel and output it. If it is not possible to do so output −1.
+
+1≤N≤105
+0≤M≤2×105
+
+Input
+
+The first line contains two integers N
+and M
+
+denoting the number of cities and the number of roads respectively.
+
+The second line contains two integers: source and target.
+
+The third line contains N
+integers which are either 0 or 1 denoting whether each city is cursed or not. (1
+
+means that the city is cursed.)
+
+Finally, there are M
+lines containing 2 integers each u and v denoting that there is a road between city u and city v
+
+.
+Output
+
+Output the smallest number of roads that you should take to complete your travel and output it. If it is not possible to do so output −1
+
+.
+Example
+Input
+
+5 5
+0 2
+0 1 0 0 0
+0 1
+1 2
+0 3
+3 4
+4 2
+
+Output
+
+3
+
+
+'''
+
+
+
+
+
+
+
 from collections import deque, defaultdict
 
 def find_shortest_path(n, m, source, target, cursed, roads):
---
+++ competititve/bad_triplet.py
@@ -0,0 +1,41 @@
+def maximum_sum(n, k, grid):
+    pairs = []
+    
+    # Consider horizontal pairs (i, j) and (i, j+1) in each row
+    for i in range(n):
+        for j in range(2):  # Only need to check (j) and (j+1) in the row
+            pairs.append((grid[i][j] + grid[i][j+1], i, j, "H"))
+    
+    # Consider vertical pairs (i, j) and (i+1, j) in each column
+    for i in range(n-1):
+        for j in range(3):
+            pairs.append((grid[i][j] + grid[i+1][j], i, j, "V"))
+    
+    # Sort pairs by the value sum in descending order
+    pairs.sort(reverse=True, key=lambda x: x[0])
+    
+    used = [[False] * 3 for _ in range(n)]  # To track used cells
+    total_sum = 0
+    placed_tiles = 0
+    
+    for value, i, j, direction in pairs:
+        if direction == "H" and j < 2 and not used[i][j] and not used[i][j+1]:
+            total_sum += value
+            used[i][j] = used[i][j+1] = True
+            placed_tiles += 1
+        elif direction == "V" and not used[i][j] and not used[i+1][j]:
+            total_sum += value
+            used[i][j] = used[i+1][j] = True
+            placed_tiles += 1
+        
+        if placed_tiles == k:
+            break
+    
+    return total_sum
+
+# Input reading
+n, k = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+# Output the result
+print(maximum_sum(n, k, grid))
